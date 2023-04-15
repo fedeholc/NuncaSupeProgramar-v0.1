4e545cee53165384770ec2d48ff51be8fd122c77
fedeholc <federicoholc@gmail.com>
2023-04-15 12:23:11 -0300

    add icons

--- blog-posts/2023-04-14-¿cómo-hacer-este-blog.md
+++ blog-posts/2023-04-14-¿cómo-hacer-este-blog.md
@@ -21,3 +21,5 @@ dominio
 dns
 
 mails
+
+react icons
--- components/navbar.js
+++ components/navbar.js
@@ -1,13 +1,27 @@
 import Logo from "./logo";
-import navbar from "./navbar.module.css"
+import navbar from "./navbar.module.css";
+import { FaGithub, FaLinkedin, FaMail } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 export default function NavBar() {
   return (
     <>
       <div className={navbar.navbar}>
         <Logo></Logo>
+        <div className={navbar.navbar__icons}>
+          <a href="https://github.com/fedeholc" target="_blank">
+            <FaGithub></FaGithub>
+          </a>
+
+          <a href="https://www.linkedin.com/in/federicoholc/" target="_blank">
+            <FaLinkedin></FaLinkedin>
+          </a>
+
+          <a href="mailto:federicoholc@gmail.com">
+            <MdEmail></MdEmail>
+          </a>
+        </div>
       </div>
-       
     </>
   );
 }
