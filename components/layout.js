44bbaa96bff97b089fd5ea64829e9e8b42f3bd33
fedeholc <federicoholc@gmail.com>
2023-04-15 09:06:38 -0300

    add logo component

--- components/layout.js
+++ components/layout.js
@@ -1,55 +1,57 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import Logo from "./logo";
 
 export const siteTitle = "<NuncaSupeProgramar/>";
 export const siteDescription =
-  "Blog de (mis) apuntes para aprender desarrollo web";
+  "Blog de apuntes para aprender desarrollo web";
 
 export default function Layout({ children, home }) {
   return (
-    <div className="layout__container">
-      <Head>
-        TODO: cambiar favicon
-        {/* <link rel="icon" href="/favicon.ico" /> */}
-        <meta
-          name="description"
-          content="Blog de apuntes para aprender a programar"
-        />
-        TODO: ¿cómo poner mi propia imagen?
-        {/*  <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        /> */}
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
-      <header className="">
-        {home ? (
-          <>
-            <h1 className="">{siteTitle}</h1> <small>{siteDescription}</small>
-            {/* TODO: poner bien la descripción, buscar tipografía, tal vez armar componente entre titulo y descripcion */}
-          </>
-        ) : (
-          <>
-            <h1>
-              <Link href="/">{siteTitle}</Link>
-            </h1>
-          </>
-        )}
-      </header>
-      <main>
-        {children}
-        {!home && (
-          <div>
-            <Link href="/">← Back to home</Link>
-          </div>
-        )}
-      </main>
-
-      <footer>foooooter</footer>
+    <div className="layout__wrapper">
+      <div className="layout__container">
+        <Head>
+          TODO: cambiar favicon
+          {/* <link rel="icon" href="/favicon.ico" /> */}
+          <meta
+            name="description"
+            content="Blog de apuntes para aprender a programar"
+          />
+          TODO: ¿cómo poner mi propia imagen?
+          {/*  <meta
+            property="og:image"
+            content={`https://og-image.vercel.app/${encodeURI(
+              siteTitle
+            )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          /> */}
+          <meta name="og:title" content={siteTitle} />
+          <meta name="twitter:card" content="summary_large_image" />
+        </Head>
+        <header className="">
+          {home ? (
+            <>
+               <Logo></Logo>
+              {/* TODO: poner bien la descripción, buscar tipografía, tal vez armar componente entre titulo y descripcion */}
+            </>
+          ) : (
+            <>
+              <h1>
+                <Link href="/">{siteTitle}</Link>
+              </h1>
+            </>
+          )}
+        </header>
+        <main>
+          {children}
+          {!home && (
+            <div>
+              <Link href="/">← Back to home</Link>
+            </div>
+          )}
+        </main>
+        <footer>foooooter</footer>
+      </div>
     </div>
   );
 }
---
+++ components/logo.js
@@ -0,0 +1,14 @@
+import logo from "./logo.module.css";
+ 
+import { siteTitle, siteDescription } from "./layout";
+
+export default function Logo() {
+  return (
+    <>
+      <div className={logo.logo__titulo}>{siteTitle}</div>
+      <div className={logo.logo__descripcion}>{siteDescription}</div>
+      
+    </>
+  );
+  
+}
