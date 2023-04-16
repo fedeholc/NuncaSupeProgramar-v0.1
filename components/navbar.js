c6b9ccf596f34391f9d676c9fb8479cd4d7038fb
fedeholc <federicoholc@gmail.com>
2023-04-16 12:42:47 -0300

    update global css

--- components/navbar.js
+++ components/navbar.js
@@ -2,24 +2,29 @@ import Logo from "./logo";
 import navbar from "./navbar.module.css";
 import { FaGithub, FaLinkedin, FaMail } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
+import Link from "next/link";
 
 export default function NavBar() {
   return (
     <>
       <div className={navbar.navbar}>
-        <Logo></Logo>
-        <div className={navbar.navbar__icons}>
+        <div className={navbar.navbar__links}>
+          <Link href="/">
+            <div className={navbar.navbar__links__fede}>Inicio</div>
+          </Link>{" "}
+          |
           <a href="https://github.com/fedeholc" target="_blank">
             <FaGithub></FaGithub>
           </a>
-
           <a href="https://www.linkedin.com/in/federicoholc/" target="_blank">
             <FaLinkedin></FaLinkedin>
           </a>
-
           <a href="mailto:federicoholc@gmail.com">
             <MdEmail></MdEmail>
           </a>
+        </div>
+        <div className={navbar.navbar__logo}>
+          <Logo></Logo>
         </div>
       </div>
     </>
